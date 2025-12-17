c938f80334fe569fa373f53aa1c7e39f5db678c2
kaveesha-dilshan <kaveeshadilshan5445@gmail.com>
2025-12-17 16:07:58 +0530

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { assets, menuLinks } from '../assets/assets'
+import { Link, useLocation } from 'react-router-dom'
+
+const Navbar = () => {
+  const location = useLocation()
+  const [open, setOpen] = useState(false)
+
+  return (
+    <div
+      className={`flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 text-gray-600 border-b border-borderColor relative transition-all ${
+        location.pathname === '/' ? 'bg-light' : ''
+      }`}
+    >
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="h-8" />
+      </Link>
+
+      {/* navigation + search */}
+      <div
+        className={`fixed top-16 right-0 h-screen w-full sm:static sm:h-auto sm:w-auto sm:top-auto sm:flex sm:flex-row
+          sm:items-center gap-4 sm:gap-8 p-4 sm:p-0 transition-all duration-300 z-50 ${
+            location.pathname === '/' ? 'bg-light' : 'bg-white'
+          } ${open ? 'translate-x-0' : 'translate-x-full'} sm:translate-x-0`}
+      >
+        {menuLinks.map((link, index) => (
+          <Link key={index} to={link.path}>
+            {link.name}
+          </Link>
+        ))}
+
+        {/* Search: visible on lg and up */}
+        <div className="hidden lg:flex items-center text-sm gap-2 border border-borderColor px-3 rounded-full max-w-56">
+          <input
+            type="text"
+            className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
+            placeholder="Search products"
+          />
+          <img src={assets.search_icon} alt="search" className="h-4 w-4" />
+        </div>
+
+        <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
+            <button className="cursor-pointer">Dashboard</button>
+            <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg">Login</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
