5b043d5a84ed0138fc902679ba81e34d3323731e
kaveesha-dilshan <kaveeshadilshan5445@gmail.com>
2025-12-18 23:44:00 +0530

    making home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { assets, menuLinks } from '../assets/assets'
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
   const location = useLocation()
   const [open, setOpen] = useState(false)
+  const navigate = useNavigate
 
   return (
     <div
@@ -18,7 +19,7 @@ const Navbar = () => {
 
       {/* navigation + search */}
       <div
-        className={`fixed top-16 right-0 h-screen w-full sm:static sm:h-auto sm:w-auto sm:top-auto sm:flex sm:flex-row
+        className={`fixed top-16 right-0 h-screen w-full flex flex-col sm:static sm:h-auto sm:w-auto sm:top-auto sm:flex sm:flex-row
           sm:items-center gap-4 sm:gap-8 p-4 sm:p-0 transition-all duration-300 z-50 ${
             location.pathname === '/' ? 'bg-light' : 'bg-white'
           } ${open ? 'translate-x-0' : 'translate-x-full'} sm:translate-x-0`}
@@ -40,10 +41,15 @@ const Navbar = () => {
         </div>
 
         <div className='flex max-sm:flex-col items-start sm:items-center gap-6'>
-            <button className="cursor-pointer">Dashboard</button>
-            <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg">Login</button>
+            <button onClick={()=> navigate('/owner')} className="cursor-pointer">Dashboard</button>
+            <button onClick={()=> setShowLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition-all text-white rounded-lg">Login</button>
         </div>
       </div>
+
+      <button className='sm:hidden cursor-pointer' aria-label="Menu" onClick={()=> setOpen(!open)}>
+        <img src={open ? assets.close_icon : assets.menu_icon} alt="menu" />
+      </button>  
+
     </div>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+
+const Home = () => {
+  return (
+    <>
+        <Hero />
+        <FeaturedSection />
+    </>
+
+  )
+}
+
+export default Home
