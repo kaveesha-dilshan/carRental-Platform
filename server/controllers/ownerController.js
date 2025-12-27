730bb6c839ad220cac76d106ae0fa9121d52357c
kaveesha-dilshan <kaveeshadilshan5445@gmail.com>
2025-12-27 11:46:29 +0530

    connect with mongoDB

---
+++ server/controllers/ownerController.js
@@ -0,0 +1,28 @@
+import User from "../models/User.js";
+
+// API to change role of user
+export const changeRoleToOwner = async (req, res,)=>{
+    try {
+        const {_id} = req.user;
+        await User.findByIdAndUpdate(_id, {role: "owner"})
+        res.json({success: true, message: "Now you can list cars"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+// API to list Car
+
+export const addCar = async (req, res)=>{
+    try {
+        const {_id} = req.user;
+        let car = JSON.parse(req.body.carData);
+        const imageFile = req.file;
+
+        
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
