5479488a31d5a55088cde1166dca8dd1f57555bf
kaveesha-dilshan <kaveeshadilshan5445@gmail.com>
2026-04-09 22:10:43 +0530

    connecting FrontEnd and BackEnd ( login connected )

--- client/src/components/owner/SideBar.jsx
+++ client/src/components/owner/SideBar.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from 'react'
-import { assets, dummyUserData, ownerMenuLinks } from '../../assets/assets'
+import { assets, ownerMenuLinks } from '../../assets/assets'
 import { NavLink, useLocation } from 'react-router-dom';
+import { useAppContext } from '../../Context/AppContext';
+import toast from 'react-hot-toast';
 
 const SideBar = () => {
 
-    const user =dummyUserData;
+    const {user, axios, fetchUser} = useAppContext()
     const location = useLocation()
     const [image, setImage] = useState('')
 
     const updateImage = async ()=> {
-        user.image = URL.createObjectURL(image)
-        setImage('')
+        try {
+            const formData = new FormData()
+            formData.append('image', image)
+
+            const {data} = await axios.post('/api/owner/update-image', formData)
+
+            if(data.success){
+                fetchUser()
+                toast.success(data.message)
+                setImage('')
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message )
+        }
     }
 
   return (
@@ -28,7 +44,7 @@ const SideBar = () => {
             </label>
         </div>
         {image && (
-            <button onClick={updateImage} className='absolute top-0 right-0 flex p-2 gap-1 bg-primary/10 text-primary cursor-pointer'>
+            <button className='absolute top-0 right-0 flex p-2 gap-1 bg-primary/10 text-primary cursor-pointer' onClick={updateImage}>
                 Save <img src={assets.check_icon} width={13} alt="" />
             </button>
         )}
